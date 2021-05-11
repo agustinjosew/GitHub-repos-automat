09d8d4ea68e92aa98a46fbc7cdc9d653d75eeb08
Agustin Jose <agustinwawrzyk@protonmail.com>
2021-05-11 09:50:45 -0300

    start branch develop

--- crear_repos.py
+++ crear_repos.py
@@ -1,11 +1,8 @@
 import requests
+from config.config import TOKEN
 from pprint import pprint
 
 
 # entry point
 if __name__ == "__main__":
 
-    API_URL = "https://api.github.com"
-    payload = '{"name":"agustinjosew"}'
-    r = requests.post(API_URL+"/user/repos", data=payload)
-    pprint(r.json())
